31cb9c0d5327229a3e2093c5c397e2bf8e611775
Julius Kiano <jgkiano@gmail.com>
2021-10-22 07:19:16 +0300

    initial testing scafold

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,55 +8,121 @@ void main() {
   runApp(const MyApp());
 }
 
+class FullButton extends StatelessWidget {
+  final void Function()? onPressed;
+  final String title;
+  const FullButton({Key? key, this.onPressed, required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(title),
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(
+          double.infinity,
+          35,
+        ), // double.infinity is the width and 30 is the height
+      ),
+    );
+  }
+}
+
+class MessageBox extends StatelessWidget {
+  final String message;
+  const MessageBox({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        color: Colors.grey.shade400,
+        border: Border.all(color: Colors.grey.shade800),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Text(message),
+      ),
+    );
+  }
+}
+
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await Okhi.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+  String message = "";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text("OkHi"),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FullButton(
+                title: "Location Services Check",
+                onPressed: _handleIsLocationServicesEnabled,
+              ),
+              FullButton(
+                title: "Location Permission Check",
+                onPressed: _handleIsLocationPermissionGranted,
+              ),
+              FullButton(
+                title: "Background Location Permission Check",
+                onPressed: _handleIsBackgroundLocationPermissionGranted,
+              ),
+              FullButton(
+                title: "Google Play Services Permission Check",
+                onPressed: _handleIsGooglePlayServicesAvailable,
+              ),
+              MessageBox(message: message)
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  _handleIsLocationServicesEnabled() async {
+    final result = await OkHi.isLocationServicesEnabled();
+    setState(() {
+      message = result.toString();
+    });
+  }
+
+  _handleIsLocationPermissionGranted() async {
+    final result = await OkHi.isLocationPermissionGranted();
+    setState(() {
+      message = result.toString();
+    });
+  }
+
+  _handleIsBackgroundLocationPermissionGranted() async {
+    final result = await OkHi.isBackgroundLocationPermissionGranted();
+    setState(() {
+      message = result.toString();
+    });
+  }
+
+  _handleIsGooglePlayServicesAvailable() async {
+    final result = await OkHi.isGooglePlayServicesAvailable();
+    setState(() {
+      message = result.toString();
+    });
   }
 }
