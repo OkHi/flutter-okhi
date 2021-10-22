9cd0613227b5ccfb7e1140d5e38c9b8c0f898b14
Julius Kiano <jgkiano@gmail.com>
2021-10-22 07:19:01 +0300

    expose static methods

--- lib/okhi.dart
+++ lib/okhi.dart
@@ -1,13 +1,36 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-class Okhi {
+class OkHi {
   static const MethodChannel _channel = MethodChannel('okhi');
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<bool> isLocationServicesEnabled() async {
+    final bool result =
+        await _channel.invokeMethod("isLocationServicesEnabled");
+    return result;
+  }
+
+  static Future<bool> isLocationPermissionGranted() async {
+    final bool result =
+        await _channel.invokeMethod("isLocationPermissionGranted");
+    return result;
+  }
+
+  static Future<bool> isBackgroundLocationPermissionGranted() async {
+    final bool result =
+        await _channel.invokeMethod("isBackgroundLocationPermissionGranted");
+    return result;
+  }
+
+  static Future<bool> isGooglePlayServicesAvailable() async {
+    final bool result =
+        await _channel.invokeMethod("isGooglePlayServicesAvailable");
+    return result;
   }
 }
