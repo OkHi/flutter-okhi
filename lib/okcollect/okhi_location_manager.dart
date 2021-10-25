8b60ec6d951f2cdc6efae34b31e9e36733b69539
Julius Kiano <jgkiano@gmail.com>
2021-10-25 11:00:16 +0300

    implemented location manager

--- lib/okcollect/okhi_location_manager.dart
+++ lib/okcollect/okhi_location_manager.dart
@@ -1,1 +1,26 @@
-class OkHiLocationManager {}
+import 'dart:io';
+import 'package:flutter/widgets.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class OkHiLocationManager extends StatefulWidget {
+  const OkHiLocationManager({Key? key}) : super(key: key);
+
+  @override
+  _OkHiLocationManagerState createState() => _OkHiLocationManagerState();
+}
+
+class _OkHiLocationManagerState extends State<OkHiLocationManager> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const WebView(
+      initialUrl: 'https://okhi.com',
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
