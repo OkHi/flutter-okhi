2652c9ae5880e8876e1d5140e2cff72af8af98fe
Julius Kiano <jgkiano@gmail.com>
2021-10-25 17:57:28 +0300

    added constants

---
+++ lib/models/okhi_constant.dart
@@ -0,0 +1,5 @@
+class OkHiConstant {
+  static String DEV_SIGN_IN_URL = "https://dev-api.okhi.io/v5/auth/anonymous-signin";
+  static String SANDBOX_SIGN_IN_URL = "https://sandbox-api.okhi.io/v5/auth/anonymous-signin";
+  static String PROD_SIGN_IN_URL = "https://api.okhi.io/v5/auth/anonymous-signin";
+}
