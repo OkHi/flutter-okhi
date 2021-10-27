36bb27bf100638848d9f3728a13b7e24dd94b422
Julius Kiano <jgkiano@gmail.com>
2021-10-27 16:34:18 +0300

    added lib version

--- lib/models/okhi_constant.dart
+++ lib/models/okhi_constant.dart
@@ -1,4 +1,5 @@
 class OkHiConstant {
+  static const String libraryVersion = "1.0.0";
   static const String devSignInUrl =
       "https://dev-api.okhi.io/v5/auth/anonymous-signin";
   static const String sandboxSignInUrl =
@@ -8,6 +9,7 @@ class OkHiConstant {
   static const String okhiLogoUrl =
       "https://storage.googleapis.com/okhi-cdn/images/logos/okhi-logo-white.png";
   static const String devLocationManagerUrl = "https://dev-manager-v5.okhi.io";
-  static const String sandboxLocationManagerUrl = "https://sandbox-manager-v5.okhi.io";
+  static const String sandboxLocationManagerUrl =
+      "https://sandbox-manager-v5.okhi.io";
   static const String prodLocationManagerUrl = "https://manager-v5.okhi.io";
 }
