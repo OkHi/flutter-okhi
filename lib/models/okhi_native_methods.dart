54f1e0caeadadecbcdd9d4cd7ed197aba3352dd6
Julius Kiano <jgkiano@gmail.com>
2021-10-27 16:33:54 +0300

    created central point for native method access

---
+++ lib/models/okhi_native_methods.dart
@@ -0,0 +1,19 @@
+class OkHiNativeMethod {
+  static const String getPlatformVersion = "getPlatformVersion";
+  static const String isLocationServicesEnabled = "isLocationServicesEnabled";
+  static const String isLocationPermissionGranted =
+      "isLocationPermissionGranted";
+  static const String isBackgroundLocationPermissionGranted =
+      "isBackgroundLocationPermissionGranted";
+  static const String isGooglePlayServicesAvailable =
+      "isGooglePlayServicesAvailable";
+  static const String requestLocationPermission = "requestLocationPermission";
+  static const String requestBackgroundLocationPermission =
+      "requestBackgroundLocationPermission";
+  static const String requestEnableLocationServices =
+      "requestEnableLocationServices";
+  static const String requestEnableGooglePlayServices =
+      "requestEnableGooglePlayServices";
+  static const String getAppIdentifier = "getAppIdentifier";
+  static const String getAppVersion = "getAppVersion";
+}
