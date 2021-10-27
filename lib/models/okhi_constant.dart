2da14b17abadca89f7a1fc0b98ed88bd57e41b8f
Julius Kiano <jgkiano@gmail.com>
2021-10-27 10:47:34 +0300

    added urls

--- lib/models/okhi_constant.dart
+++ lib/models/okhi_constant.dart
@@ -1,6 +1,13 @@
 class OkHiConstant {
-  static const String devSignInUrl = "https://dev-api.okhi.io/v5/auth/anonymous-signin";
-  static const String sandboxSignInUrl = "https://sandbox-api.okhi.io/v5/auth/anonymous-signin";
-  static const String prodSignInUrl = "https://api.okhi.io/v5/auth/anonymous-signin";
-  static const String okhiLogoUrl = "https://storage.googleapis.com/okhi-cdn/images/logos/okhi-logo-white.png";
+  static const String devSignInUrl =
+      "https://dev-api.okhi.io/v5/auth/anonymous-signin";
+  static const String sandboxSignInUrl =
+      "https://sandbox-api.okhi.io/v5/auth/anonymous-signin";
+  static const String prodSignInUrl =
+      "https://api.okhi.io/v5/auth/anonymous-signin";
+  static const String okhiLogoUrl =
+      "https://storage.googleapis.com/okhi-cdn/images/logos/okhi-logo-white.png";
+  static const String devLocationManagerUrl = "https://dev-manager-v5.okhi.io";
+  static const String sandboxLocationManagerUrl = "https://sandbox-manager-v5.okhi.io";
+  static const String prodLocationManagerUrl = "https://manager-v5.okhi.io";
 }
