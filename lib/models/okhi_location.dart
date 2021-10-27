788663fe260db0093698cad681b08051ee17f3f3
Julius Kiano <jgkiano@gmail.com>
2021-10-27 12:44:31 +0300

    created okhi location class

---
+++ lib/models/okhi_location.dart
@@ -0,0 +1,71 @@
+class OkHiLocation {
+  String? id;
+  double? lat;
+  double? lon;
+  String? city;
+  String? country;
+  String? directions;
+  String? displayTitle;
+  String? otherInformation;
+  String? photoUrl;
+  String? placeId;
+  String? plusCode;
+  String? propertyName;
+  String? propertyNumber;
+  String? state;
+  String? streetName;
+  String? streetViewPanoId;
+  String? streetViewPanoUrl;
+  String? subtitle;
+  String? title;
+  String? url;
+  String? userId;
+
+  OkHiLocation({
+    this.id,
+    this.lat,
+    this.lon,
+    this.city,
+    this.country,
+    this.directions,
+    this.displayTitle,
+    this.otherInformation,
+    this.photoUrl,
+    this.placeId,
+    this.plusCode,
+    this.propertyName,
+    this.propertyNumber,
+    this.state,
+    this.streetName,
+    this.streetViewPanoId,
+    this.streetViewPanoUrl,
+    this.subtitle,
+    this.title,
+    this.url,
+    this.userId,
+  });
+
+  OkHiLocation.fromMap(Map<String, dynamic> data) {
+    id = data["id"];
+    lat = data["geo_point"] != null ? data["geo_point"]["lat"] : null;
+    lon = data["geo_point"] != null ? data["geo_point"]["lon"] : null;
+    city = data["city"];
+    country = data["country"];
+    directions = data["directions"];
+    displayTitle = data["display_title"];
+    otherInformation = data["other_information"];
+    photoUrl = data["photo"];
+    placeId = data["place_id"];
+    plusCode = data["plus_code"];
+    propertyName = data["property_name"];
+    propertyNumber = data["property_number"];
+    state = data["state"];
+    streetName = data["street_name"];
+    streetViewPanoId = data["street_view_pano_id"];
+    streetViewPanoUrl = data["street_view_pano_url"];
+    subtitle = data["subtitle"];
+    title = data["title"];
+    url = data["url"];
+    userId = data["user_id"];
+  }
+}
