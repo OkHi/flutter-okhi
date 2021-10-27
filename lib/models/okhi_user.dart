f073208752bd3887f231e03be1d25378d08b5eab
Julius Kiano <jgkiano@gmail.com>
2021-10-27 12:44:46 +0300

    added fromMap constructor

--- lib/models/okhi_user.dart
+++ lib/models/okhi_user.dart
@@ -3,5 +3,12 @@ class OkHiUser {
   String? lastName;
   String? id;
   String phone;
+
   OkHiUser({required this.phone, this.firstName, this.lastName, this.id});
+  
+  OkHiUser.fromMap({required this.phone, required Map<String, String> data}) {
+    id = data["id"];
+    firstName = data["firstName"] ?? data["first_name"];
+    lastName = data["lastName"] ?? data["last_name"];
+  }
 }
