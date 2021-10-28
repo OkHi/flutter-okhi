060446ea936ca2f12cd10be280e41df966c151a7
Julius Kiano <jgkiano@gmail.com>
2021-10-28 10:59:25 +0300

    override default notifications

--- lib/models/okhi_app_configuration.dart
+++ lib/models/okhi_app_configuration.dart
@@ -1,3 +1,5 @@
+import './okhi_notification.dart';
+
 import './okhi_env.dart';
 
 class OkHiAppConfiguration {
@@ -5,18 +7,26 @@ class OkHiAppConfiguration {
   final String clientKey;
   OkHiEnv env = OkHiEnv.sandbox;
   String environmentRawValue = "sandbox";
+  OkHiAndroidNotification notification = OkHiAndroidNotification.withDefaults();
 
   OkHiAppConfiguration({
     required this.branchId,
     required this.clientKey,
     required this.env,
+    OkHiAndroidNotification? notification,
   }) {
     if (env == OkHiEnv.prod) {
       environmentRawValue = "prod";
     }
+    this.notification = notification ?? OkHiAndroidNotification.withDefaults();
   }
-  OkHiAppConfiguration.withRawValue(
-      {required this.branchId,
-      required this.clientKey,
-      required this.environmentRawValue});
+
+  OkHiAppConfiguration.withRawValue({
+    required this.branchId,
+    required this.clientKey,
+    required this.environmentRawValue,
+    OkHiAndroidNotification? notification
+  }) {
+    this.notification = notification ?? OkHiAndroidNotification.withDefaults();
+  }
 }
