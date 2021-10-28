76426d347cc359008329e8a13e1aa2e84e7df909
Julius Kiano <jgkiano@gmail.com>
2021-10-28 10:59:57 +0300

    made button handle states

--- example/lib/widgets/full_button.dart
+++ example/lib/widgets/full_button.dart
@@ -3,21 +3,30 @@ import 'package:flutter/material.dart';
 class FullButton extends StatelessWidget {
   final void Function()? onPressed;
   final String title;
-  const FullButton({Key? key, this.onPressed, required this.title})
-      : super(key: key);
+  final bool dis;
+  const FullButton({Key? key, this.onPressed, required this.title, disabled})
+      : dis = disabled ?? false,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     return ElevatedButton(
-      onPressed: onPressed,
+      onPressed: _handlePress,
       child: Text(title),
       style: ElevatedButton.styleFrom(
+        primary: dis ? Colors.grey.shade300 : Colors.lightBlue,
         minimumSize: Size(
           width,
           35,
         ), // double.infinity is the width and 30 is the height
       ),
     );
+  }
+
+  _handlePress() {
+    if (!dis && onPressed != null) {
+      onPressed!();
+    }
   }
 }
