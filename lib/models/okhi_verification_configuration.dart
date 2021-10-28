5233c106a19a4b64c8b6d64d99093433f452a6c0
Julius Kiano <jgkiano@gmail.com>
2021-10-28 10:58:16 +0300

    created verification configuration class

---
+++ lib/models/okhi_verification_configuration.dart
@@ -0,0 +1,6 @@
+class OkHiVerificationConfiguration {
+  bool withForegroundService = true;
+  OkHiVerificationConfiguration({bool? withForegroundService}) {
+    this.withForegroundService = withForegroundService ?? true;
+  }
+}
