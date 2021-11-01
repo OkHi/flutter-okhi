2424e90096fc35013ccea5782ea88921bb7d8db2
Julius Kiano <jgkiano@gmail.com>
2021-11-01 21:15:30 +0300

    added canStartVerification

--- lib/okhi.dart
+++ lib/okhi.dart
@@ -6,6 +6,7 @@ import 'package:okhi/models/okhi_user.dart';
 import './models/okhi_app_configuration.dart';
 import './models/okhi_native_methods.dart';
 import './models/okhi_verification_configuration.dart';
+import './models/okhi_exception.dart';
 
 // models export
 export './okcollect/okhi_location_manager.dart';
@@ -17,6 +18,7 @@ export './models/okhi_location_manager_configuration.dart';
 export './models/okhi_notification.dart';
 export './models/okhi_verification_configuration.dart';
 export './models/okhi_location.dart';
+export './models/okhi_exception.dart';
 
 class OkHi {
   static const MethodChannel _channel = MethodChannel('okhi');
@@ -52,9 +54,9 @@ class OkHi {
           .invokeMethod(OkHiNativeMethod.isBackgroundLocationPermissionGranted);
       return result;
     } else {
-      // ignore: todo
-      // TODO: work on OkHiException class
-      throw Exception('Platform not supported');
+      throw OkHiException(
+          code: OkHiException.unsupportedPlatformCode,
+          message: OkHiException.unsupportedPlatformMessage);
     }
   }
 
@@ -87,8 +89,10 @@ class OkHi {
           .invokeMethod(OkHiNativeMethod.requestEnableGooglePlayServices);
       return result;
     } else {
-      // ignore: todo
-      throw Exception('Platform not supported');
+      throw OkHiException(
+        code: OkHiException.unsupportedPlatformCode,
+        message: OkHiException.unsupportedPlatformMessage,
+      );
     }
   }
 
@@ -111,27 +115,29 @@ class OkHi {
   static Future<String> startVerification(OkHiUser user, OkHiLocation location,
       OkHiVerificationConfiguration? configuration) async {
     if (location.id == null || location.lat == null || location.lon == null) {
-      // ignore: todo
-      // TODO: error handling
-      throw Exception("missing values");
-    } else {
-      final config = configuration ?? OkHiVerificationConfiguration();
-      return await _channel.invokeMethod(OkHiNativeMethod.startVerification, {
-        "phoneNumber": user.phone,
-        "locationId": location.id,
-        "lat": location.lat,
-        "lon": location.lon,
-        "withForegroundService": config.withForegroundService,
-      });
+      throw OkHiException(
+        code: OkHiException.badRequestCode,
+        message: "Invalid arguments provided for starting verification",
+      );
     }
+
+    final config = configuration ?? OkHiVerificationConfiguration();
+    return await _channel.invokeMethod(OkHiNativeMethod.startVerification, {
+      "phoneNumber": user.phone,
+      "locationId": location.id,
+      "lat": location.lat,
+      "lon": location.lon,
+      "withForegroundService": config.withForegroundService,
+    });
   }
 
   static Future<String> stopVerification(
       OkHiUser user, OkHiLocation location) async {
     if (location.id == null) {
-      // ignore: todo
-      // TODO: error handling
-      throw Exception("missing values");
+      throw OkHiException(
+        code: OkHiException.badRequestCode,
+        message: "Invalid arguments provided for stopping verification",
+      );
     } else {
       return await _channel.invokeMethod(OkHiNativeMethod.stopVerification, {
         "phoneNumber": user.phone,
@@ -151,5 +157,29 @@ class OkHi {
 
   static Future<bool> stopForegroundService() async {
     return await _channel.invokeMethod(OkHiNativeMethod.stopForegroundService);
+  }
+
+  static Future<bool> canStartVerification(bool requestServices) async {
+    if (Platform.isIOS && !(await OkHi.isLocationServicesEnabled())) {
+      throw OkHiException(
+        code: OkHiException.serviceUnavailableCode,
+        message: "Location services disabled",
+      );
+    }
+    var hasLocationServices = Platform.isIOS ? true : await OkHi.isLocationServicesEnabled();
+    var hasLocationPermission =
+        await OkHi.isBackgroundLocationPermissionGranted();
+    var hasGooglePlayService =
+        Platform.isIOS ? true : await OkHi.isGooglePlayServicesAvailable();
+    if (!requestServices) {
+      return hasLocationServices &&
+          hasLocationPermission &&
+          hasGooglePlayService;
+    }
+    hasLocationServices = await OkHi.requestEnableLocationServices();
+    hasLocationPermission = await OkHi.requestBackgroundLocationPermission();
+    hasGooglePlayService =
+        Platform.isIOS ? true : await OkHi.requestEnableGooglePlayServices();
+    return hasLocationServices && hasLocationPermission && hasGooglePlayService;
   }
 }
